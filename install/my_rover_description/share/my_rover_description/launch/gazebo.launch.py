0773a0d18d4c82072003031f07df7b600eb1893a
rahul <rahul.stemeducator@gmail.com>
2026-05-15 09:08:27 +0530

    Added complete ros2 rover workspace with ros2 controll for gazebo and rviz

---
+++ install/my_rover_description/share/my_rover_description/launch/gazebo.launch.py
@@ -0,0 +1,82 @@
+from launch import LaunchDescription
+from pathlib import Path
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command,LaunchConfiguration
+from launch.actions import TimerAction
+
+
+
+def generate_launch_description():
+
+    rover_description_dir = get_package_share_directory("my_rover_description")
+
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(get_package_share_directory("my_rover_description"), "urdf", "rover.urdf.xacro"),
+        description="Absolute path to robot URDF file"
+    )
+
+    robot_description = ParameterValue(Command(["xacro ",LaunchConfiguration("model")]), value_type=str)
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
+    )
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(rover_description_dir).parent.resolve())
+        ]
+    )
+
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource([
+        os.path.join(
+            get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+        launch_arguments=[
+            ("gz_args", [" -v 4"," -r"," empty.sdf"])
+        ]
+    )
+    
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-topic", "robot_description",
+                   "-name", "simplerover"]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"]
+    )
+
+    diff_drive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller"]
+)
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher,
+        gazebo_resource_path,
+        gazebo,
+        gz_spawn_entity,
+
+        TimerAction(
+            period=5.0,
+            actions=[joint_state_broadcaster_spawner]
+        ),
+
+        TimerAction(
+            period=7.0,
+            actions=[diff_drive_controller_spawner]
+        )
+])
