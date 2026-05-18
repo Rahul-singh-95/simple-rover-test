9aadb52d05099b0721c7a184b9995ae823359d05
rahul <rahul.stemeducator@gmail.com>
2026-05-18 11:03:24 +0530

    Added rover description and rover bringup package

--- build/my_rover_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rahul/simple_rover/src/my_rover_bringup
-# Build directory: /home/rahul/simple_rover/build/my_rover_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
